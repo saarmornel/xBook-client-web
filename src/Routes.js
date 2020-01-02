633597f89ba19ad29011875ffd9000a9d4932e41
SirSaar <saar643@gmail.com>
2020-01-02 10:26:08 +0200

    fix

--- src/Routes.js
+++ src/Routes.js
@@ -6,54 +6,51 @@ import Giveaway from './screens/Giveaway';
 import Reading from './screens/Reading';
 import Requests from './screens/Requests';
 import SignIn from './screens/SignIn';
-import authStore from './stores/auth.store';
 
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
+const Routes = (props) => {
+  
+  const PrivateRoute = ({ children, ...rest }) => {
     return (
       <Route
         {...rest}
         render={({ location }) =>
-        authStore.loadToken() ? (
+          props.authStore.loadToken() ? (
             children
           ) : (
-            <Redirect
-              to={{
-                pathname: "/sign_in",
-                state: { from: location }
-              }}
-            />
-          )
+              <Redirect
+                to={{
+                  pathname: "/sign_in",
+                  state: { from: location }
+                }}
+              />
+            )
         }
       />
     );
   }
 
-
-const Routes = () => {
-    return (
-        <Switch>
-            <PrivateRoute exact path="/explore">
-                <Explore></Explore>
-            </PrivateRoute>
-            <PrivateRoute exact path="/add_book">
-                <AddBook></AddBook>
-            </PrivateRoute>
-            <PrivateRoute exact path="/reading">
-                <Reading></Reading>
-            </PrivateRoute>
-            <PrivateRoute exact path="/giveaway">
-                <Giveaway></Giveaway>
-            </PrivateRoute>
-            <PrivateRoute exact path="/requests">
-                <Requests></Requests>
-            </PrivateRoute>
-            <Route exact path="/sign_in">
-                <SignIn></SignIn>
-            </Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <PrivateRoute exact path="/explore">
+        <Explore></Explore>
+      </PrivateRoute>
+      <PrivateRoute exact path="/add_book">
+        <AddBook></AddBook>
+      </PrivateRoute>
+      <PrivateRoute exact path="/reading">
+        <Reading></Reading>
+      </PrivateRoute>
+      <PrivateRoute exact path="/giveaway">
+        <Giveaway></Giveaway>
+      </PrivateRoute>
+      <PrivateRoute exact path="/requests">
+        <Requests></Requests>
+      </PrivateRoute>
+      <Route exact path="/sign_in">
+        <SignIn></SignIn>
+      </Route>
+    </Switch>
+  );
 };
 
-export default Routes;
+export default inject('authStore')(observer(Routes));
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -7,46 +7,46 @@ import {
     Redirect,
     useHistory,
     useLocation
-  } from "react-router-dom";
+} from "react-router-dom";
 import { inject, observer } from 'mobx-react';
 
 const qs = require('query-string');
 
-const SignIn = inject('authStore')(observer(
+const SignIn =
     (props) => {
-    let history = useHistory();
-    let location = useLocation();
-    const authToken = qs.parse(location.search, { ignoreQueryPrefix: true }).auth_token;
+        let history = useHistory();
+        let location = useLocation();
+        const authToken = qs.parse(location.search, { ignoreQueryPrefix: true }).auth_token;
 
-    const onFailed = async () => {
-        console.log('auth failed');
-    }
-    
-    const onSuccess = async (token) => {
-        console.log('auth succeeded');
-        await props.authStore.setToken(token);
-        history.replace('/explore')
-    }
-
-    console.log(authToken)
-    if(authToken) {
-        if(authToken==='null') {
-            onFailed();
-        } else {
-            onSuccess();
+        const onFailed = async () => {
+            console.log('auth failed');
         }
+
+        const onSuccess = async (token) => {
+            console.log('auth succeeded');
+            await props.authStore.setToken(token);
+            history.replace('/explore')
+        }
+
+        console.log(authToken)
+        if (authToken) {
+            if (authToken === 'null') {
+                onFailed();
+            } else {
+                onSuccess();
+            }
+        }
+
+
+
+        return (
+            <div>
+                <p>You must log in to view the page</p>
+                <a href="https://glacial-fortress-14735.herokuapp.com/api/auth/facebook/" > Login with Facebook</a>
+            </div>
+        );
     }
 
 
 
-    return (
-        <div>
-            <p>You must log in to view the page</p>
-            <a href="https://glacial-fortress-14735.herokuapp.com/api/auth/facebook/" > Login with Facebook</a>
-        </div>
-    );
-    }
-));
-
-
-export default SignIn;
+export default inject('authStore')(observer(SignIn))
