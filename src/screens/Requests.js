fcbc07d2f7d8ffb64d874035662a7465329e83ec
SirSaar <saar643@gmail.com>
2020-01-13 14:56:31 +0200

    refactor

--- src/screens/Requests.js
+++ src/screens/Requests.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        hidden={value !== index}
+        id={index}
+        {...other}
+      >
+        {value === index && children}
+      </Typography>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+
+const useStyles = makeStyles(theme => ({
+root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+},
+}));
 
 const Requests = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
     return (
-        <div>
-            
-        </div>
-    );
+        <div className={classes.root}>
+            <AppBar >
+                <Tabs value={value} onChange={handleChange} centered>
+                    <Tab label="Incoming" id="incoming" />
+                    <Tab label="Outgoing" id="outgoing" />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+            Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+            Item Two
+            </TabPanel>
+      </div>
+      );
 };
 
 export default Requests;
