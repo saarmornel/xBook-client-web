4ea3bfe23470c16fb0d13f000fa84ba22c4e443b
SirSaar <saar643@gmail.com>
2020-01-19 17:53:28 +0200

    fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const IS_DEV = true;
-export const serverUrl = IS_DEV ? 'http://localhost':'https://glacial-fortress-14735.herokuapp.com';
+const isDev = false;
+export const serverUrl = isDev ? 'http://localhost' : 'https://glacial-fortress-14735.herokuapp.com/';
 export const authCookie = 'authToken';
