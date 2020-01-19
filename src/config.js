13473112e91120e54aedc27fb345e5e1af146dea
Lord Saruman <saar643@gmail.com>
2020-01-19 18:08:52 +0200

    fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 const isDev = false;
-export const serverUrl = isDev ? 'http://localhost' : 'https://glacial-fortress-14735.herokuapp.com/';
+export const serverUrl = isDev ? 'http://localhost' : 'https://glacial-fortress-14735.herokuapp.com';
 export const authCookie = 'authToken';
