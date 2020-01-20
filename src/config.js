a4e29b589223025b9612bca885229a6421ce7e71
SirSaar <saar643@gmail.com>
2020-01-20 10:50:57 +0200

    feat: env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const isDev = false;
-export const serverUrl = isDev ? 'http://localhost' : 'https://glacial-fortress-14735.herokuapp.com';
+const isLocal = process.env.REACT_APP_ENV === 'local' ? true : false;
+export const serverUrl = isLocal ? 'http://localhost' : 'https://glacial-fortress-14735.herokuapp.com';
 export const authCookie = 'authToken';
+
+console.log('isLocal:',true)
