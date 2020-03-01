fa2fb4372ed39744e748e3f117de6b8f3a256a69
SirSaar <saar643@gmail.com>
2020-03-01 12:43:03 +0200

    fix

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -30,7 +30,7 @@ const BottomNavbar = () => {
           <BottomNavigationAction component={Link} to="/app/explore" value="explore" label="Explore" icon={<Search />}></BottomNavigationAction>
           <BottomNavigationAction component={Link} to="/app/giveaway" label="Giveaway" value="giveaway" icon={<LibraryBooks />}></BottomNavigationAction>  
           <BottomNavigationAction component={Link} to="/app/requests" label="Requests" value="requests" icon={<SwapCalls />}></BottomNavigationAction>
-          <BottomNavigationAction component={Link} to="/app/settings" label="Settings" value="settings" icon={<Settings />}></BottomNavigationAction>
+          {/* <BottomNavigationAction component={Link} to="/app/settings" label="Settings" value="settings" icon={<Settings />}></BottomNavigationAction> */}
         </BottomNavigation>
     );
 };
