bcdf6062220f49df9a79249241ec6a660fe73144
SirSaar <saar643@gmail.com>
2020-03-01 10:11:49 +0200

    feat: add google books link

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import { Hidden,Divider } from '@material-ui/core';
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
+
 
 const image = {height: 160,width: 100 }
 
@@ -45,8 +35,10 @@ const BookCard = ({data,header,actions}) => {
 
                 <div className={classes.content}>
                   <CardContent>
-                    <Typography noWrap component="h5" variant="subtitle1"> 
-                        {title}
+                    <Typography noWrap component="h5" variant="subtitle1">
+                        <Link href={more}>
+                          {title}
+                        </Link>
                     </Typography>
                     <Typography noWrap variant="subtitle1" color="textSecondary">
                         by {author}
